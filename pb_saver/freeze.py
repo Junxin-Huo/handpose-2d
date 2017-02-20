8571b2d83b6a8a93f50009814c14a08514c376c3
1352861513 <1352861513@qq.com>
2017-02-20 17:21:51 +0800

    Add files via upload

---
+++ pb_saver/freeze.py
@@ -0,0 +1,19 @@
+from freeze_graph import freeze_graph
+
+input_graph = '../data/train.pb'
+input_saver = ''
+input_binary = True
+input_checkpoint = '../data/net.ckpt'
+output_node_names = 'h_hidden3,argmax'
+restore_op_name = "save/restore_all"
+filename_tensor_name = "save/Const:0"
+output_graph = "../data/output_graph.pb"
+clear_devices = True
+initializer_nodes = ''
+
+print "freezing..."
+freeze_graph(input_graph, input_saver,
+        input_binary, input_checkpoint,
+        output_node_names, restore_op_name,
+        filename_tensor_name, output_graph,
+        clear_devices, "") 
